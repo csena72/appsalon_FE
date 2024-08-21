e45b9e35c4e4a07a783602d6fddfdd9053778d3e
Cristian Sena <crisianjsena@gmail.com>
2024-08-21 14:55:47 -0300

    seleccion de servicios

--- src/components/ServiceItem.vue
+++ src/components/ServiceItem.vue
@@ -1,6 +1,8 @@
 <script setup>
-
     import { formatCurrency } from '@/helpers'
+    import { useAppointmentsStore } from '@/stores/appointments'
+
+    const appointments = useAppointmentsStore()
 
     defineProps({
         service: {
@@ -11,11 +13,16 @@
 
 <template>
     <div
-        class="p-5 space-y-5 rounded-lg cursor-pointer bg-white hover:bg-gray-600"
+        class="p-5 space-y-5 rounded-lg cursor-pointer"
+        @click="appointments.onServiceSelected(service)"
+        :class="appointments.isServiceSelected(service._id) ? 'bg-blue-500 text-white' : 'bg-white'"
     >
         <p class="text-2xl font-light">{{ service.name }}</p>
 
-        <p class="text-4xl font-black text-blue-600">{{ formatCurrency(service.price) }}</p>
+        <p
+            :class="appointments.isServiceSelected(service._id) ? 'text-white' : 'text-blue-600'"
+            class="text-4xl font-black ">{{ formatCurrency(service.price) }}
+        </p>
     </div>
 </template>
 
